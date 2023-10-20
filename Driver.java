5da69e081b938e073c425ee5722da9a5a04af7f5
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-10-19 23:59:38 -0400

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,23 @@
+public class Driver{
+  public static void main(String[]args){
+    /* Write your tests here
+    * Make sure to write more than 3 test cases
+    * Three or less test cases will lower your grade
+    * When your create tests cases try to test extreme cases (something that is not commmon, but it could happen)
+    * When your create tests cases, you should test all the methods you coded in your class Poimt
+    */ 
+
+    /*****************     TEST CASE 00    ******************/
+    Point p1 = new Point(8.0, 8.0);
+    Point p2 = new Point(2.0, 10.0);
+    Point p3 = new Point(0.0, 0.0);
+
+    // Print coordinates using your toString method from Point class
+    
+    // Calculate distances and print results
+    
+  
+    /*************     ADD MORE TEST CASES HERE    *************/
+
+  }
+}
